8895a2e3c264fbbdb4ed6dc39000f94128cb4f58
liguang <guang.li@dmall.com>
2026-06-30 22:40:09 +0800

    测试全局依赖

--- config/dependencies/auth.py
+++ config/dependencies/auth.py
@@ -1,3 +1,12 @@
 # 全局校验
-async def verify_token():
+from typing import Annotated
+
+from fastapi import Depends
+from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
+
+get_bearer_token = HTTPBearer(auto_error=False)
+
+
+async def verify_token(credentials: Annotated[HTTPAuthorizationCredentials, Depends(get_bearer_token)]):
     print("校验token")
+    print(f"当前Token: {credentials}")
